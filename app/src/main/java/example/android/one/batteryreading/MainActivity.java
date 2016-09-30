449860b6c50418ca40737de96766fc5eb2675edc
Pedro Borges <pedroborges@onesource.pt>
2016-09-30 14:47:16 +0100

    no message

---
+++ app/src/main/java/example/android/one/batteryreading/MainActivity.java
@@ -0,0 +1,29 @@
+package example.android.one.batteryreading;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryStatus = getApplicationContext().registerReceiver(null, ifilter);
+
+        TextView tView = (TextView) findViewById(R.id.textView);
+
+        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        float batteryPct = level / (float)scale;
+
+        tView.setText(String.valueOf(batteryPct));
+    }
+}
